71f2608b993ce8694fe8c0016a352758d30ed482
IyuninVA <IyuninVA@bk.ru>
2024-02-16 03:28:51 +0400

    v1.00

--- Programs/Сonfiguration/GetFile.java
+++ Programs/Сonfiguration/GetFile.java
@@ -7,9 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 import Data.User;
 import Data.Directory.GetDirectory;
 import Views.ViewConfig;
